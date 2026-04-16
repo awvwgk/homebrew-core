d30e708866a089c2f6898e3faeacce731320a9e4
Johan Eckerström <johan@duh.se>
2026-04-16 14:39:02 +0200

    lavinmq 2.7.2 (new formula)

---
+++ Formula/l/lavinmq.rb
@@ -0,0 +1,60 @@
+class Lavinmq < Formula
+  desc "Message broker implementing the AMQP 0-9-1 and MQTT protocols"
+  homepage "https://lavinmq.com"
+  url "https://github.com/cloudamqp/lavinmq/archive/refs/tags/v2.7.2.tar.gz"
+  sha256 "c5fed6406e48df516daa5b3dc29d8f6fae6549cb0971ed21293e197260c67879"
+  license "Apache-2.0"
+  head "https://github.com/cloudamqp/lavinmq.git", branch: "main"
+
+  depends_on "crystal" => :build
+  depends_on "help2man" => :build
+  depends_on "bdw-gc"
+  depends_on "lz4"
+  depends_on "openssl@3"
+  depends_on "pcre2"
+
+  on_macos do
+    # GNU install (Makefile uses `install -D -t`); Linux's /usr/bin/install is already GNU.
+    depends_on "coreutils" => :build
+  end
+
+  on_linux do
+    depends_on "pkgconf" => :build
+    depends_on "zlib-ng-compat"
+  end
+
+  def install
+    ENV.prepend_path "PATH", Formula["coreutils"].opt_libexec/"gnubin" if OS.mac?
+
+    inreplace "extras/lavinmq.ini", /^data_dir.*/, "data_dir = #{var}/lavinmq"
+
+    system "make", "install",
+           "DOCS=",
+           "PREFIX=#{prefix}",
+           "SYSCONFDIR=#{buildpath}/stage/etc",
+           "UNITDIR=#{buildpath}/stage/systemd",
+           "SYSUSERSDIR=#{buildpath}/stage/sysusers",
+           "SHAREDSTATEDIR=#{buildpath}/stage/var"
+
+    pkgetc.install "extras/lavinmq.ini"
+  end
+
+  service do
+    run [opt_bin/"lavinmq", "-c", etc/"lavinmq/lavinmq.ini"]
+    keep_alive true
+  end
+
+  test do
+    pid = spawn bin/"lavinmq", "--data-dir", testpath/"data"
+    30.times do
+      break if File.exist?("/tmp/lavinmqctl.sock")
+
+      sleep 1
+    end
+    output = shell_output("#{bin}/lavinmqctl status")
+    assert_match "Uptime", output
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
