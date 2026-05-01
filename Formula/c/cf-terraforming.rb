3a1d4a5323edbae11e03e4ec75a2a6bbc91cdac2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-01 20:37:13 +0000

    cf-terraforming: update 0.26.0 bottle.

--- Formula/c/cf-terraforming.rb
+++ Formula/c/cf-terraforming.rb
@@ -7,12 +7,12 @@ class CfTerraforming < Formula
   head "https://github.com/cloudflare/cf-terraforming.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e93ec88b3189a64a77c0805b35d95a194a7912d04f64401ab71b0e38fadd7914"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e93ec88b3189a64a77c0805b35d95a194a7912d04f64401ab71b0e38fadd7914"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e93ec88b3189a64a77c0805b35d95a194a7912d04f64401ab71b0e38fadd7914"
-    sha256 cellar: :any_skip_relocation, sonoma:        "86d8cf1b9b05f502f373a4c9e00d146a94e3be839123a1053544a4a2d7b027f3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1ae12af08c0543567168513cdd819bf6b45c8c33f88760f0a0d4a634f1ad34f7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b392ebde697f31704e4cf26f52e27e9a82a8696654dc5eacdac9e6b66b8ece70"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8db1dbbef5153721bfd2b7433066140b47fe75534236e4f1f76b500d2d41b0b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8db1dbbef5153721bfd2b7433066140b47fe75534236e4f1f76b500d2d41b0b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8db1dbbef5153721bfd2b7433066140b47fe75534236e4f1f76b500d2d41b0b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b49067d58b3cdf602dc8fdc532334293358e0afcf3de42f20733df16906651f1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1479a298af9d8205d42318c5eb61145d93fb964e9046776ba38445393c176483"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ba3b2b3c64d6bb58debdb0b30bd7648f21627e85e68f84e994b16b874da46155"
   end
 
   depends_on "go" => :build
