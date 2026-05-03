a061ab5354a306be7426600af6498e72752aa1cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-03 14:57:41 +0000

    ugrep 7.8.1

--- Formula/u/ugrep.rb
+++ Formula/u/ugrep.rb
@@ -1,8 +1,8 @@
 class Ugrep < Formula
   desc "Ultra fast grep with query UI, fuzzy search, archive search, and more"
   homepage "https://ugrep.com/"
-  url "https://github.com/Genivia/ugrep/archive/refs/tags/v7.8.0.tar.gz"
-  sha256 "8563eb731f206187c6a0e0652be1cdb8afdf368ddb2e48ba2b25c0b06d8b0aa6"
+  url "https://github.com/Genivia/ugrep/archive/refs/tags/v7.8.1.tar.gz"
+  sha256 "204473f377bdfd2aa2c9fa71074dad799e1848d91510d41c2f36ec0edcf5bc43"
   license "BSD-3-Clause"
 
   bottle do
