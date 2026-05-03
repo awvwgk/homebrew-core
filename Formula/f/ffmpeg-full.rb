4b8664570b4a5f3902a7a732bffffa7c5dae5d97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-03 23:22:07 +0000

    ffmpeg-full 8.1.1

--- Formula/f/ffmpeg-full.rb
+++ Formula/f/ffmpeg-full.rb
@@ -1,13 +1,12 @@
 class FfmpegFull < Formula
   desc "Play, record, convert, and stream many audio and video codecs"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-8.1.tar.xz"
-  sha256 "b072aed6871998cce9b36e7774033105ca29e33632be5b6347f3206898e0756a"
+  url "https://ffmpeg.org/releases/ffmpeg-8.1.1.tar.xz"
+  sha256 "b6863adde98898f42602017462871b5f6333e65aec803fdd7a6308639c52edf3"
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   # Passing `--enable-version3` changes the license to GPL v3+.
   license "GPL-3.0-or-later"
-  revision 1
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   livecheck do
