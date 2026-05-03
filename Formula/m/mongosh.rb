3662306829b07cdec0fd7fb199136f2b689a300a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-03 05:19:29 +0000

    mongosh: update 2.8.3 bottle.

--- Formula/m/mongosh.rb
+++ Formula/m/mongosh.rb
@@ -9,12 +9,12 @@ class Mongosh < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d67cc5c70961c77a5c3ecf6aeac41082d7623310f1458f00ce5e55b625e2a0a6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d67cc5c70961c77a5c3ecf6aeac41082d7623310f1458f00ce5e55b625e2a0a6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d67cc5c70961c77a5c3ecf6aeac41082d7623310f1458f00ce5e55b625e2a0a6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "483a78843385fe722bf8c07ac6e936f90ce4f3ed0b59a1295a8cb8b950047b57"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0d38657b01483b66c75e0d436ec172972b6d2c2e33eb2e4876bae098048b549d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0d38657b01483b66c75e0d436ec172972b6d2c2e33eb2e4876bae098048b549d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3d14d7ee324a8f078aa8909cab4cef5cb99d22b3d6e15e45e9b61f70baeb4975"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d14d7ee324a8f078aa8909cab4cef5cb99d22b3d6e15e45e9b61f70baeb4975"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d14d7ee324a8f078aa8909cab4cef5cb99d22b3d6e15e45e9b61f70baeb4975"
+    sha256 cellar: :any_skip_relocation, sonoma:        "672ec3ce969fe94f3679587e0b2ab9cd7d5aed80992a05be1b160ebde1d74689"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c00f0f02b049d44581e9ae5fc43fb89531920dbe75f9cd262e54f40da8bcd9c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c00f0f02b049d44581e9ae5fc43fb89531920dbe75f9cd262e54f40da8bcd9c"
   end
 
   depends_on "node"
