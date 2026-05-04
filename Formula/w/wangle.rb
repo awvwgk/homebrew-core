de45ffef391feee9c181abf1ae43930b97d08187
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 20:35:01 +0000

    wangle 2026.05.04.00

--- Formula/w/wangle.rb
+++ Formula/w/wangle.rb
@@ -1,8 +1,8 @@
 class Wangle < Formula
   desc "Modular, composable client/server abstractions framework"
   homepage "https://github.com/facebook/wangle"
-  url "https://github.com/facebook/wangle/archive/refs/tags/v2026.04.27.00.tar.gz"
-  sha256 "ade67be4fd362d88f2c184ed2e05729af6ac9579f4f7281a902af1e0c41f8125"
+  url "https://github.com/facebook/wangle/archive/refs/tags/v2026.05.04.00.tar.gz"
+  sha256 "3eda2852781edf9ba6e9361f2de51633d1754f75f902cb4b49f483ebb403a8a5"
   license "Apache-2.0"
   compatibility_version 1
   head "https://github.com/facebook/wangle.git", branch: "main"
