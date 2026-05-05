b92ef523193fe7ac91b4fded17460a444c341c63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-05 09:34:42 +0000

    tomee-webprofile 10.1.5

--- Formula/t/tomee-webprofile.rb
+++ Formula/t/tomee-webprofile.rb
@@ -1,9 +1,9 @@
 class TomeeWebprofile < Formula
   desc "All-Apache Java EE 7 Web Profile stack"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-10.1.4/apache-tomee-10.1.4-webprofile.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-10.1.4/apache-tomee-10.1.4-webprofile.tar.gz"
-  sha256 "388b1e351ab92e50c1100bbdc1a208352359111a994b40b837300f44ca95b389"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-10.1.5/apache-tomee-10.1.5-webprofile.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-10.1.5/apache-tomee-10.1.5-webprofile.tar.gz"
+  sha256 "53a2da3872bbc20f21570bb9ba6b4c23243d57a31df2b016bb23bbc7fe831a5f"
   license "Apache-2.0"
 
   bottle do
