351c7dfedc081f75b6fbccaa0ed373b756e67d33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-06 06:04:39 +0000

    fresh-editor 0.3.4

--- Formula/f/fresh-editor.rb
+++ Formula/f/fresh-editor.rb
@@ -1,8 +1,8 @@
 class FreshEditor < Formula
   desc "Text editor for your terminal: easy, powerful and fast"
   homepage "https://sinelaw.github.io/fresh/"
-  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "39b3a5c3cfe2dd6af7e2d202faa33275d71796826fa9b9590daaf96558585691"
+  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "9c493f2af15d2b87b672a67a3701b2e92a0835537fe171e3c0102224484d1d36"
   license "GPL-2.0-or-later"
 
   bottle do
