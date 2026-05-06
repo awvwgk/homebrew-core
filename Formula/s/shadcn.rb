bfa8bfe4a1ac2816db48b9f7126a618b7977477d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-06 15:40:28 +0000

    shadcn 4.7.0

--- Formula/s/shadcn.rb
+++ Formula/s/shadcn.rb
@@ -1,8 +1,8 @@
 class Shadcn < Formula
   desc "CLI for adding components to your project"
   homepage "https://ui.shadcn.com"
-  url "https://registry.npmjs.org/shadcn/-/shadcn-4.6.0.tgz"
-  sha256 "bc865a26ea52d516c0ba752ec51540eadc5691170b54d709891d205832d5b729"
+  url "https://registry.npmjs.org/shadcn/-/shadcn-4.7.0.tgz"
+  sha256 "5d2155e7015f74c55552cbcfde798ab336d693d05690461bfa83db4aea09b203"
   license "MIT"
 
   bottle do
