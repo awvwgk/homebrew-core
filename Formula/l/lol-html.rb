723b05b3c2cf2bb3a54a1f17aa8c99ba386c46e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-06 18:58:11 +0000

    lol-html: update 2.9.0 bottle.

--- Formula/l/lol-html.rb
+++ Formula/l/lol-html.rb
@@ -7,12 +7,12 @@ class LolHtml < Formula
   head "https://github.com/cloudflare/lol-html.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "7517ed962bb6167a9ea9ddf812b8cbb485d766eb61b09bb90bf38e2ca30cc83c"
-    sha256 cellar: :any,                 arm64_sequoia: "5f807653830c3b7d3fd3a4a78ec4fff4a82791eecff8ad17e44bb208a6b09bb2"
-    sha256 cellar: :any,                 arm64_sonoma:  "cf11e71d2e93bcc118e0c56093c50cb52677b92adfb6bb0af68c81e2c34f9302"
-    sha256 cellar: :any,                 sonoma:        "29cdaf5c902675db95894b3456782b4ec51d4a5abcb51dbc035f57ed24273d62"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "947d9d4859883980005abe4f441992b052672715867f580771f50acf453da62a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df33145b452fdcfb3b5192fb3dfed0b6fea383558d066228b6bd4a99ee5b7b93"
+    sha256 cellar: :any,                 arm64_tahoe:   "8cfa473b47043e5b2b9f79d0ef76a4bc564342f99909203a091e7182b3017f73"
+    sha256 cellar: :any,                 arm64_sequoia: "da4120648589c454a6ac76a242cde093bb38d0318c7139d4848f2b8d3cb7ae12"
+    sha256 cellar: :any,                 arm64_sonoma:  "378c0e3b8b3df82070dcc465fbaa5b7e26834895f50395fcd492911e03f88386"
+    sha256 cellar: :any,                 sonoma:        "518c5176e97ab58006a6da50a8a8fe7565a292b5dd7dec6edf7787d026cffd00"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a0458e14c42e720c5fb1bea25746ce145e89358cabdb816c622d0e36c4c19118"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a390425b7cc7005dd41a87c1eed2659d05038562d170629bdf1b0473b4d74513"
   end
 
   depends_on "cargo-c" => :build
