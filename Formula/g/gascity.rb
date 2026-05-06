f9386f905d877fab2a1951cbd487c2a80debd5c3
Julian Knutsen <julianknutsen@users.noreply.github.com>
2026-05-06 21:19:55 +0000

    gascity 1.1.0 (new formula)

---
+++ Formula/g/gascity.rb
@@ -0,0 +1,47 @@
+class Gascity < Formula
+  desc "Orchestration-builder SDK for multi-agent coding workflows"
+  homepage "https://github.com/gastownhall/gascity"
+  url "https://github.com/gastownhall/gascity/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "ae37e7dcc31e6ca3db435940c305bff36510dd2e32dbf472260c3eedaab0826c"
+  license "MIT"
+  head "https://github.com/gastownhall/gascity.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "beads"
+  depends_on "dolt"
+  depends_on "jq"
+  depends_on "tmux"
+
+  on_macos do
+    depends_on "flock"
+  end
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:, output: bin/"gc"), "./cmd/gc"
+  end
+
+  test do
+    (testpath/"city-template.toml").write <<~TOML
+      [workspace]
+      name = "brew-test"
+
+      [beads]
+      provider = "file"
+    TOML
+
+    ENV["GC_HOME"] = testpath/".gc-home"
+    city = testpath/"brew-city"
+
+    output = shell_output("#{bin}/gc init --skip-provider-readiness --file city-template.toml #{city} 2>&1", 1)
+    assert_match "Initialized city \"brew-city\"", output
+    assert_path_exists city/"city.toml"
+    assert_path_exists city/"pack.toml"
+    assert_path_exists city/".gc/beads.json"
+    assert_match "name = \"brew-city\"", (city/".gc/site.toml").read
+    assert_match "provider = \"file\"", (city/"city.toml").read
+  end
+end
