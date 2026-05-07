651a3c1f76053cd1fe75fbb80da4d8c30374d9b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-07 20:38:58 +0000

    hyper-mcp 0.6.1

--- Formula/h/hyper-mcp.rb
+++ Formula/h/hyper-mcp.rb
@@ -1,8 +1,8 @@
 class HyperMcp < Formula
   desc "MCP server that extends its capabilities through WebAssembly plugins"
   homepage "https://github.com/hyper-mcp-rs/hyper-mcp"
-  url "https://github.com/hyper-mcp-rs/hyper-mcp/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "29bc87410a3df6db6d2c0c21093bea5645fea919a68c5122b8b07367d95907a5"
+  url "https://github.com/hyper-mcp-rs/hyper-mcp/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "571b7c6b8b95d98a679cf2413cf4328b0538f6eebcd34a8825f771b59788c664"
   license "Apache-2.0"
   head "https://github.com/hyper-mcp-rs/hyper-mcp.git", branch: "main"
 
