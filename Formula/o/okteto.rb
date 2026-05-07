72f7ccb17396f2243217d89f83a450b785d32a16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-07 11:59:24 +0000

    okteto 3.19.0

--- Formula/o/okteto.rb
+++ Formula/o/okteto.rb
@@ -1,8 +1,8 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/refs/tags/3.18.0.tar.gz"
-  sha256 "b8a75709827c009048df112de626602b2cbe89bf89fa9c7ab5ae989e9cfa6cd7"
+  url "https://github.com/okteto/okteto/archive/refs/tags/3.19.0.tar.gz"
+  sha256 "3bb13c8740112262bfb006ff02435b0d3c7b52d47c5c49788e05b2844fa27fc0"
   license "Apache-2.0"
   head "https://github.com/okteto/okteto.git", branch: "master"
 
