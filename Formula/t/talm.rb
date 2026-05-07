582e2fcf945e0048e5d504f6e4c0eef9b24f291f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-07 13:00:43 +0000

    talm: update 0.25.0 bottle.

--- Formula/t/talm.rb
+++ Formula/t/talm.rb
@@ -7,12 +7,12 @@ class Talm < Formula
   head "https://github.com/cozystack/talm.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bdeb5228e591cb8e4ac566a85a67b5a15d5a8f4e329cebf24ca0cadc199a810d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f769da8840d79a13ae64661020c8b91365478159e13013e498f790322d2a2a0b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dfad8df9cddb049ed68e1a484f2c0a4a4b5c25679e0588300c545a13517dc138"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6228d729db750531b59ca26b0c45d50c5432501c2466a7ced98d66985e89528b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dcb566c6c60c101c8969aeb886b82ef07a841f42c6f53b85ef2efae3bd40bc1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9c5b87537f15d20b51fbacd195a58a510977501b4a36095c4d2d41ab03377f0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ce1f49a973ad203a9208135039f0f495091dfa8d497e0d01c15c73b4953ce333"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "25114e3cbb5003bf00ad94ead87c8a55cf0684abeb26bc03200da0fea703dcea"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "67b408f448a47aacd42985e88fc2e453c9c0e2803bc72ceda70d579d0533f889"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a53762c6aed186c89fad4ce2af7ce1b77e1f36c344fca4494b0b22b22baf809f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d23ce01bc6fa81307ae1f265985a0d202da1085e093ab949437da86fe3b05203"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1afcfe9bb20d8114bc706123f1a484fd6a2a4eeb7cd860aa0b976e82f8f6201f"
   end
 
   depends_on "go" => :build
