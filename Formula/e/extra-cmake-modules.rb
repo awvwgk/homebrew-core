41d4882dbf03d917e728d2efc44f69ecb583fb8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-08 14:41:54 +0000

    extra-cmake-modules 6.26.0

--- Formula/e/extra-cmake-modules.rb
+++ Formula/e/extra-cmake-modules.rb
@@ -1,8 +1,8 @@
 class ExtraCmakeModules < Formula
   desc "Extra modules and scripts for CMake"
   homepage "https://api.kde.org/ecm/"
-  url "https://download.kde.org/stable/frameworks/6.25/extra-cmake-modules-6.25.0.tar.xz"
-  sha256 "ffeb4951bf09b2b8ee17ef24a28b6de650c0db0f59ddffb9009b37cac63af348"
+  url "https://download.kde.org/stable/frameworks/6.26/extra-cmake-modules-6.26.0.tar.xz"
+  sha256 "f4e10d9d45aafb5273e996196040f4e420f0bc4071c208282aae94d9ad8e1743"
   license all_of: ["BSD-2-Clause", "BSD-3-Clause", "MIT"]
   head "https://invent.kde.org/frameworks/extra-cmake-modules.git", branch: "master"
 
