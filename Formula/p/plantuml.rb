ebaa4ce104a36b1027bbf369b5575c82402aae12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-08 17:55:30 +0000

    plantuml 1.2026.3

--- Formula/p/plantuml.rb
+++ Formula/p/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://github.com/plantuml/plantuml/releases/download/v1.2026.2/plantuml-1.2026.2.jar"
-  sha256 "3cdce52133c424dea22425b947ae9d47f2167b0866dfcf99e714d4ea1689975c"
+  url "https://github.com/plantuml/plantuml/releases/download/v1.2026.3/plantuml-1.2026.3.jar"
+  sha256 "53af6760d96bb2737e5e4386e832b46339fc29dec74f412d7c12db7c30db8ec4"
   license "GPL-3.0-or-later"
   version_scheme 1
 
