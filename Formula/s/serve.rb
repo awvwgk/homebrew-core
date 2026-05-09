2345a811ca943e4a542b3b7210b1f8211cda6276
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-09 07:01:10 +0000

    serve: update 0.7.1 bottle.

--- Formula/s/serve.rb
+++ Formula/s/serve.rb
@@ -7,12 +7,12 @@ class Serve < Formula
   head "https://github.com/syntaqx/serve.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cfce8144afb255617770210763548d39d828e46e19fee3dfe4c9781fdbd8c25d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cfce8144afb255617770210763548d39d828e46e19fee3dfe4c9781fdbd8c25d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cfce8144afb255617770210763548d39d828e46e19fee3dfe4c9781fdbd8c25d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ceaec060d2d211186be17f5129efc138b9feb2b4588c8c8fb81cd34335a4b05c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ff0a3dc0f2934c910d54fb62e916de939aaa4e18ae0ce5852b68ed4c62270ccf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ee9d0d611aaea85fbb2362847f4ff7da499ddf59a5429ff202f86995ddcb1079"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f20f905bcb40868152269a0d134dac246557d0e9860ce6f0c47e5f769d24a642"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f20f905bcb40868152269a0d134dac246557d0e9860ce6f0c47e5f769d24a642"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f20f905bcb40868152269a0d134dac246557d0e9860ce6f0c47e5f769d24a642"
+    sha256 cellar: :any_skip_relocation, sonoma:        "eadbc704bb266fd7da9459b77dad9dc59b0e2145328cf344b294ca11a5d92238"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eb00fce358a6a0dd07a029325cca6abf5a1ad79adec846e5ecf85c5e905da7db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea8df586110143308c957a2d99e8324fab02ab72e95180bf99ce7e2ee73e6c7c"
   end
 
   depends_on "go" => :build
