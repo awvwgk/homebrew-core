851688baad01d52e9b5c73ede3485581389acd78
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-09 20:52:13 +0000

    talm 0.27.0

--- Formula/t/talm.rb
+++ Formula/t/talm.rb
@@ -1,8 +1,8 @@
 class Talm < Formula
   desc "Manage Talos Linux configurations the GitOps way"
   homepage "https://github.com/cozystack/talm"
-  url "https://github.com/cozystack/talm/archive/refs/tags/v0.26.1.tar.gz"
-  sha256 "2150a7680e2e6baacde1f7c08d4d8aed632f75c7c2166120bcb1b65f8772fac5"
+  url "https://github.com/cozystack/talm/archive/refs/tags/v0.27.0.tar.gz"
+  sha256 "7b517f1529ee4e4ebba1e8f202b7a4fad6a51043e98eca0f57f0c0f9265798b4"
   license "Apache-2.0"
   head "https://github.com/cozystack/talm.git", branch: "main"
 
