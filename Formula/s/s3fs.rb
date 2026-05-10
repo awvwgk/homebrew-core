201429107f45231c6e924610b5fb83efa4eca436
Michael Cho <michael@michaelcho.dev>
2026-05-10 14:40:15 -0400

    s3fs: use OpenSSL to match libcurl
    https://github.com/s3fs-fuse/s3fs-fuse/wiki/Installation-Notes#common-for-building1-2
    
    Also revision bump to avoid broken linkage after Nettle 4

--- Formula/s/s3fs.rb
+++ Formula/s/s3fs.rb
@@ -4,6 +4,7 @@ class S3fs < Formula
   url "https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v1.97.tar.gz"
   sha256 "28413457cbf923b9b81e546caffabb8edd5c18f263e698ad86f564fd4b5b344d"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/s3fs-fuse/s3fs-fuse.git", branch: "master"
 
   bottle do
@@ -15,16 +16,14 @@ class S3fs < Formula
   depends_on "automake" => :build
   depends_on "pkgconf" => :build
   depends_on "curl"
-  depends_on "gnutls"
   depends_on "libfuse"
-  depends_on "libgcrypt"
   depends_on "libxml2"
   depends_on :linux # on macOS, requires closed-source macFUSE
-  depends_on "nettle"
+  depends_on "openssl@3"
 
   def install
     system "./autogen.sh"
-    system "./configure", "--with-gnutls", *std_configure_args
+    system "./configure", "--with-openssl", *std_configure_args
     system "make", "install"
   end
 
