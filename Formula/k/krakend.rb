e9dc4e48be330537909dfd8ac966853a724755ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-11 15:06:53 +0000

    krakend 2.13.5

--- Formula/k/krakend.rb
+++ Formula/k/krakend.rb
@@ -1,8 +1,8 @@
 class Krakend < Formula
   desc "Ultra-High performance API Gateway built in Go"
   homepage "https://www.krakend.io/"
-  url "https://github.com/krakend/krakend-ce/archive/refs/tags/v2.13.4.tar.gz"
-  sha256 "c919ab2be3aa72c8204b7658e5e7f29e4978a21763c9e287dc2af0329ddea07f"
+  url "https://github.com/krakend/krakend-ce/archive/refs/tags/v2.13.5.tar.gz"
+  sha256 "81afd8bafb732d95a635df22600088a93fc4318baceed5058dd0923d729b72fa"
   license "Apache-2.0"
   head "https://github.com/krakend/krakend-ce.git", branch: "master"
 
