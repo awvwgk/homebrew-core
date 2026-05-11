6282103ccca7bbbb82437b764059d83ce15c17b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-11 23:58:17 +0000

    localai: update 4.2.1 bottle.

--- Formula/l/localai.rb
+++ Formula/l/localai.rb
@@ -7,12 +7,12 @@ class Localai < Formula
   head "https://github.com/mudler/LocalAI.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7f450d770c8230702f07294062e201aa745f93647a732d22c958c86f9b310989"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6765df3004a1993f81db5cad09c8be754860d131d899378ddebe0d21b7905f54"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "22702cb5ec3850e1ced17e6f5dd455857f5f8cce53bf8d669031a29583d33170"
-    sha256 cellar: :any_skip_relocation, sonoma:        "50f42e6c7959604240b6f6476d66065ce683a51fcf5f8a59db5ba04a4cb5b3aa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d12ad905172a8e4132dbbee517657bd6d7c4a23d8988c507b114b2061838994f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c5b0d37e8c92143c634c2323f999e927f0ed4f00562e34b006e0832315e74cc"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "45d085bc3fa63dd51c3dc7b63a511f2954f11cc0057ccfeeeaaf4ad309fb4f5c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3e35b7a747b1d3a1573f76e5c06ebf2f5b1f518e2d3919a88c8e12bea397573d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "805c55a9a0274861e932d2fa07cbd830219b5b2290d4a262f51b6950b4ceac08"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ca429e83f0c3cbc7655a71e76b3342910dd4456c95a870973e501a25c2864c2b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4e911df77f1097888870e817edf5f0f63f1904719d858fa4b9973fa2079ca6e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c910b735d49133f924fe731d5fde9994466a46b500e7441bfbbb49ea0bc3120b"
   end
 
   depends_on "go" => :build
