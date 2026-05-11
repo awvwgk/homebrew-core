d57f10a87a7a6de426d34660c41714b813304cbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-11 00:44:30 +0000

    terraform-docs: update 0.24.0 bottle.

--- Formula/t/terraform-docs.rb
+++ Formula/t/terraform-docs.rb
@@ -6,12 +6,12 @@ class TerraformDocs < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "af19d1faefaf680609418e7bbc567f7054f17fb43426442f76b11b10ed813215"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "19a9ab9a7727151d2598c487d84980894587da637d1457962a1653fe748455af"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3b5bc5f0c6f0295b07dd316222d790e1b1dbab37b6cb294177606b0cde4cc894"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9543364f640bcc242f36def029ab283d0044b903d14329a7e49cbb446cfcc64c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "56f4cdde669690deeb326dcbb543b087f70aa7a284a0b601dc08ed08661a0e8e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a9546f48eb996ec23376ea4a142631b26371a5b2dcae155ca574ab5f607c8fd0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fa2cc1277c6cc50f860807570e9bc88868fd24e99e6d68a9d5b4a47ba25dc3cc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "42b365054923bac534510d13e6b06e7066dae1a431d0d88b67587ad70bb52077"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8305233ffe535dc775e5bbd8d8a00c2dc2a5a3133ec719e920ae65bc54d9e1af"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d23f09b9c5ff28efc122d3ef26dfc768aa58d92bc40a13fe291c0a7815901426"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1a52f38be0c6f3af1b3dad3c7e13026b20f3ee6ace1b9e5ca9be6d5dfc826f84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21e204b1283a4e3de6fb2aaf6e95edb7486aa6f3424cd0493bbd1ec396ac6997"
   end
 
   depends_on "go" => :build
