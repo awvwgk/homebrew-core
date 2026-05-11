515e1d699e5f48895f19fbc634fd56ca36fc3431
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-11 18:07:37 +0000

    opendht: update 3.7.1_1 bottle.

--- Formula/o/opendht.rb
+++ Formula/o/opendht.rb
@@ -15,12 +15,12 @@ class Opendht < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "0fc3ce493601f30db6e3a6a15c7b56cbfa0d90f911e4c0b3d4d33cd80a1b5323"
-    sha256 cellar: :any,                 arm64_sequoia: "6b3ac403c5d69df2aa6353ce7460dcacab6262a1378a3ac90bb549d791449c5f"
-    sha256 cellar: :any,                 arm64_sonoma:  "d6abe4cf6538c04ef0924ef51396c33ebce1cdc824dff800ee3a2e4492a56900"
-    sha256 cellar: :any,                 sonoma:        "bac677e8a34fd7f609412a32724e7ea59c985845c9d4b1931f5ece1e2e771ff2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "29bbab9a704134cafded477704180ef11f4f983f8f56974b97a50d2c8481e510"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3b65244d50d8ff23e65173fd246c0e93006543f19b888c16245924b87a126aeb"
+    sha256 cellar: :any,                 arm64_tahoe:   "4780c887120eb5982b7c56d8dffcf9c379742bd6d29651a45b498b1add0b1174"
+    sha256 cellar: :any,                 arm64_sequoia: "cecf455aa1a7ca788fd7aa93f61991306892eac5d4f7068262679b8913ca39e1"
+    sha256 cellar: :any,                 arm64_sonoma:  "fc85fed2611c68a15f6b415c56a4e04d0437d041cabae939c1be94d4e97b8fea"
+    sha256 cellar: :any,                 sonoma:        "594ebd908f73bd3f1c27e8c2c165a59065960adc8683af28b3f442122d956cf7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "950bd7aad0db1887175b147b1675c515932fa61a33460813d7af3ebf0c0a07a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0f7e95aebe4cee7fec6609edb149c5728cdeaa41936ae365fc4a2277e0cc3f2"
   end
 
   depends_on "cmake" => :build
