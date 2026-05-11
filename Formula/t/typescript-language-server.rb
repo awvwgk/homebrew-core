6e7084ec599dd02f9196207c0791f2ed0dd06365
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-11 18:45:00 +0000

    typescript-language-server: update 5.2.0 bottle.

--- Formula/t/typescript-language-server.rb
+++ Formula/t/typescript-language-server.rb
@@ -6,7 +6,7 @@ class TypescriptLanguageServer < Formula
   license all_of: ["MIT", "Apache-2.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f54c2455ee01697826248c2c801ec8d4bdbb7a38a0aa0a06dd8f021be327e1d6"
+    sha256 cellar: :any_skip_relocation, all: "87984bc5e744c85b7d5192611ccad2fbc299196d21e48e335c057014b323409d"
   end
 
   depends_on "node"
