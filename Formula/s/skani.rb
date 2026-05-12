353cee37378780f7a0bf2f36ddd87c811bdf73a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 18:42:32 +0000

    skani 0.3.2

--- Formula/s/skani.rb
+++ Formula/s/skani.rb
@@ -1,8 +1,8 @@
 class Skani < Formula
   desc "Fast, robust ANI and aligned fraction for (metagenomic) genomes and contigs"
   homepage "https://github.com/bluenote-1577/skani"
-  url "https://github.com/bluenote-1577/skani/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "494b64ec764be1ad485cd5d4f8c97180ff61377863d990532f6fa9541ab1b6f4"
+  url "https://github.com/bluenote-1577/skani/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "5cae2fc3b8c57881fd9d3494c372eb8c8703eb69900513bfaef01f8892c55ae0"
   license "MIT"
   head "https://github.com/bluenote-1577/skani.git", branch: "main"
 
