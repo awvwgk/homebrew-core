c470ce38c3575eabab69c8cbdd3bb075c2abe8fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 15:34:52 +0000

    vulkan-extensionlayer 1.4.350.0

--- Formula/v/vulkan-extensionlayer.rb
+++ Formula/v/vulkan-extensionlayer.rb
@@ -1,8 +1,8 @@
 class VulkanExtensionlayer < Formula
   desc "Layer providing Vulkan features when native support is unavailable"
   homepage "https://github.com/KhronosGroup/Vulkan-ExtensionLayer"
-  url "https://github.com/KhronosGroup/Vulkan-ExtensionLayer/archive/refs/tags/vulkan-sdk-1.4.341.0.tar.gz"
-  sha256 "9845b8c85099fab61efe932372be7af374532df0f1135ab25604f2363e35d864"
+  url "https://github.com/KhronosGroup/Vulkan-ExtensionLayer/archive/refs/tags/vulkan-sdk-1.4.350.0.tar.gz"
+  sha256 "ff0e13f11d13ae260acd30e257d2e71d42cb2b2d7e95de92c6861a7c077e54be"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-ExtensionLayer.git", branch: "main"
 
