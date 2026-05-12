6cbdbaa729dc7c905a3ba5335f52e53a980d38db
Michael Cho <michael@michaelcho.dev>
2026-05-12 15:33:25 -0400

    shellinabox: migrate to `openssl@4`

--- Formula/s/shellinabox.rb
+++ Formula/s/shellinabox.rb
@@ -18,7 +18,7 @@ class Shellinabox < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   on_linux do
     depends_on "zlib-ng-compat"
@@ -37,7 +37,7 @@ class Shellinabox < Formula
     ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1403
 
     system "autoreconf", "--force", "--install", "--verbose"
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
