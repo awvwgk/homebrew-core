375e96ec03478237e2445373a73d4de97411890a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 20:58:09 +0000

    monero 0.18.5.0

--- Formula/m/monero.rb
+++ Formula/m/monero.rb
@@ -1,8 +1,8 @@
 class Monero < Formula
   desc "Official Monero wallet and CPU miner"
   homepage "https://www.getmonero.org/downloads/#cli"
-  url "https://downloads.getmonero.org/cli/monero-source-v0.18.4.6.tar.bz2"
-  sha256 "86668243beb87ffee3eed0a76723e4ed8a7cffd797fa59ebc2722cfc84c916a5"
+  url "https://downloads.getmonero.org/cli/monero-source-v0.18.5.0.tar.bz2"
+  sha256 "c764dfdf6d710c8dea913e77f31d0c75a8c6a3710a448341d28c3688ad2384e5"
   license "BSD-3-Clause"
 
   livecheck do
