9826ea71d4fb87d73b73df09369c95d483523502
Michael Cho <michael@michaelcho.dev>
2026-05-13 08:20:32 -0400

    code-cli: migrate to `openssl@4`

--- Formula/c/code-cli.rb
+++ Formula/c/code-cli.rb
@@ -22,7 +22,7 @@ class CodeCli < Formula
 
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   on_linux do
     depends_on "zlib-ng-compat"
@@ -30,10 +30,12 @@ class CodeCli < Formula
 
   conflicts_with cask: "visual-studio-code"
 
+  def openssl = Formula["openssl@4"]
+
   def install
     # Ensure that the `openssl` crate picks up the intended library.
     # https://crates.io/crates/openssl#manual-configuration
-    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
+    ENV["OPENSSL_DIR"] = openssl.opt_prefix
 
     ENV["VSCODE_CLI_NAME_LONG"] = "Code OSS"
     ENV["VSCODE_CLI_VERSION"] = version
@@ -51,8 +53,8 @@ class CodeCli < Formula
     assert_match version.to_s, shell_output("#{bin}/code --version")
 
     linked_libraries = [
-      Formula["openssl@3"].opt_lib/shared_library("libssl"),
-      Formula["openssl@3"].opt_lib/shared_library("libcrypto"),
+      openssl.opt_lib/shared_library("libssl"),
+      openssl.opt_lib/shared_library("libcrypto"),
     ]
 
     linked_libraries.each do |library|
