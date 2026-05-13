c34eea362aed178a0a66c64446a7d91a8c5103bb
Leon Adomaitis <leon@adomaitis.de>
2026-05-13 22:04:12 +0200

    fortitude 0.9.0

--- Formula/f/fortitude.rb
+++ Formula/f/fortitude.rb
@@ -1,8 +1,8 @@
 class Fortitude < Formula
   desc "Fortran linter"
   homepage "https://fortitude.readthedocs.io/en/stable/"
-  url "https://github.com/PlasmaFAIR/fortitude/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "54a9a12f0b0f85f1448c75d4204b9eb5d2c3ef2095abb364716cd884b65e553b"
+  url "https://github.com/PlasmaFAIR/fortitude/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "04018554aadd9b846c99b5ffc218fe483c353020daefb777af39a75724a6338e"
   license "MIT"
   head "https://github.com/PlasmaFAIR/fortitude.git", branch: "main"
 
@@ -17,6 +17,10 @@ class Fortitude < Formula
 
   depends_on "rust" => :build
 
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args(path: "crates/fortitude")
   end
@@ -30,10 +34,7 @@ class Fortitude < Formula
       ENDPROGRAM
     FORTRAN
 
-    output = shell_output("#{bin}/fortitude check #{testpath}/test.f90 2>&1", 1)
-    assert_match <<~EOS, output
-      fortitude: 1 files scanned.
-      Number of errors: 2
-    EOS
+    output = shell_output("#{bin}/fortitude check #{testpath}/test.f90 --output-format concise 2>&1", 1)
+    assert_match "fortitude: 1 files scanned.\nNumber of errors: 5", output
   end
 end
