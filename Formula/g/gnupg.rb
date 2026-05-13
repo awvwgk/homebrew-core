5ecc6ad7222d5e3ed635c41cc05c732f7c5e234e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 15:00:04 +0000

    gnupg 2.5.20

--- Formula/g/gnupg.rb
+++ Formula/g/gnupg.rb
@@ -1,8 +1,8 @@
 class Gnupg < Formula
   desc "GNU Privacy Guard (OpenPGP)"
   homepage "https://gnupg.org/"
-  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.5.19.tar.bz2"
-  sha256 "722aa8a426dd9b44e0d194b73bfee3a3e617d65674cd4d1d062e6df29f1788c6"
+  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.5.20.tar.bz2"
+  sha256 "6461266e99c308419a379abe6c356d54c214136c4589bd65951091138989ffc6"
   license "GPL-3.0-or-later"
   compatibility_version 1
 
