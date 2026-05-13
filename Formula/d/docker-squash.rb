76baf4aa21f03292b337c1ab574d01f2087b8cdd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 09:57:15 +0000

    docker-squash: update 1.2.2_6 bottle.

--- Formula/d/docker-squash.rb
+++ Formula/d/docker-squash.rb
@@ -9,7 +9,7 @@ class DockerSquash < Formula
   revision 6
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "dfdbdc5187c10cdb4dff17f4012f2f97d80e6114f520dec8c0224b32f7d743bd"
+    sha256 cellar: :any_skip_relocation, all: "fa8d7cc9c909c0e657ba78be9289bf8077bc5e571e9801265b1db9e6d245b841"
   end
 
   depends_on "certifi"
