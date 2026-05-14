55f1186d9e9fcc54d19cfc927539a876317a89a2
Michael Cho <michael@michaelcho.dev>
2026-05-14 15:59:15 -0400

    elfutils: switch to official program names with `eu-` prefix
    Also update license based on elfutils.spec

--- Formula/e/elfutils.rb
+++ Formula/e/elfutils.rb
@@ -3,7 +3,11 @@ class Elfutils < Formula
   homepage "https://fedorahosted.org/elfutils/"
   url "https://sourceware.org/elfutils/ftp/0.195/elfutils-0.195.tar.bz2"
   sha256 "37629fdf7f1f3dc2818e138fca2b8094177d6c2d0f701d3bb650a561218dc026"
-  license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-only"]
+  license all_of: [
+    "GPL-3.0-or-later", # programs
+    { any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"] }, # libraries
+    "GFDL-1.3-no-invariants-or-later", # eu-readelf.1
+  ]
   compatibility_version 1
 
   livecheck do
@@ -29,7 +33,6 @@ class Elfutils < Formula
       --disable-silent-rules
       --disable-libdebuginfod
       --disable-debuginfod
-      --program-prefix=elfutils-
       --with-bzlib
       --with-lzma
       --with-zlib
@@ -38,9 +41,22 @@ class Elfutils < Formula
     system "./configure", *args, *std_configure_args
     system "make"
     system "make", "install"
+
+    # Create temporary compatibility executables for previous Homebrew-specific names.
+    # Remove them after 2 minor releases, i.e. 0.197
+    odie "Remove compatibility scripts!" if version >= "0.197"
+    bin.glob("eu-*") do |path|
+      old_cmd = path.basename.to_s.sub("eu-", "elfutils-")
+      (bin/old_cmd).write <<~SHELL
+        #!/bin/bash
+        echo "WARNING: #{old_cmd} has been renamed to #{path.basename}; #{old_cmd} will be removed in 0.197" >&2
+        exec "#{path}" "$@"
+      SHELL
+    end
   end
 
   test do
-    assert_match "elf_kind", shell_output("#{bin}/elfutils-nm #{bin}/elfutils-nm")
+    assert_match "elf_kind", shell_output("#{bin}/eu-nm #{bin}/eu-nm")
+    assert_match "elf_kind", shell_output("#{bin}/elfutils-nm #{bin}/eu-nm")
   end
 end
