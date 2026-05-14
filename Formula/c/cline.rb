645d7bd9dc5aabb6c1ddeb0759ad9c68b7c4b79d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-14 18:00:35 +0000

    cline: update 3.0.2 bottle.

--- Formula/c/cline.rb
+++ Formula/c/cline.rb
@@ -6,12 +6,12 @@ class Cline < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256                               arm64_tahoe:   "d053568917c056c8c3a8937e58f0b5f2688d40b7c16fe2f8ea53b97d537a8203"
-    sha256                               arm64_sequoia: "d053568917c056c8c3a8937e58f0b5f2688d40b7c16fe2f8ea53b97d537a8203"
-    sha256                               arm64_sonoma:  "d053568917c056c8c3a8937e58f0b5f2688d40b7c16fe2f8ea53b97d537a8203"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ff35da4dacfdcdde0f9983ec4a6c47aacd01915b6b839d536499015e8f11c903"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ea26df0f447cb2cec9c5a34cab606a93c4e910579dd9062e70e1ec3ddb547e4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fd33a6099ae247ddd66fe9c91abdf659e93d90d21de1355b34e5b8936631967a"
+    sha256                               arm64_tahoe:   "2ad4488da60715cff5769b969360d92bbe6bef653ae4c41733097e4f0b425e7d"
+    sha256                               arm64_sequoia: "2ad4488da60715cff5769b969360d92bbe6bef653ae4c41733097e4f0b425e7d"
+    sha256                               arm64_sonoma:  "2ad4488da60715cff5769b969360d92bbe6bef653ae4c41733097e4f0b425e7d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0c16d34dbb70507923e039e28a18db83f08701d2a1daeb95d74ece6ffe23c4c3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "26d53204bbabe0a1518bf9e76dd1a5e49d6f3733719701f6115d0a3a802a72ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6a403bcaa7590ec6b0b30ab0eb28a62995a8988e8309ad27199bda90501c95e"
   end
 
   depends_on "node"
