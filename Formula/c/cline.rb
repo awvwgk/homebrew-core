69662926cf607b3f331f9c951a354583698701af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-14 17:29:53 +0000

    cline 3.0.2

--- Formula/c/cline.rb
+++ Formula/c/cline.rb
@@ -1,8 +1,8 @@
 class Cline < Formula
   desc "AI-powered coding agent for complex work"
   homepage "https://cline.bot"
-  url "https://registry.npmjs.org/cline/-/cline-3.0.0.tgz"
-  sha256 "3fc5758453a3c7d7647b08984e92676349f88708cd593232728694b349ab8dee"
+  url "https://registry.npmjs.org/cline/-/cline-3.0.2.tgz"
+  sha256 "fae5699ffbbebf3d3852a17da2e03273b5f36d9bf5ac30a0efff386336056754"
   license "Apache-2.0"
 
   bottle do
