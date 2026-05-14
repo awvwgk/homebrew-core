df26eacff9263ed7f5b4e083ee86d1723def4c8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-14 12:55:30 +0000

    circumflex: update 4.1 bottle.

--- Formula/c/circumflex.rb
+++ Formula/c/circumflex.rb
@@ -7,12 +7,12 @@ class Circumflex < Formula
   head "https://github.com/bensadeh/circumflex.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f221aab5645f12b7d8187a21881906aabfdaa7e11c13e6facd5baea0c804b889"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f221aab5645f12b7d8187a21881906aabfdaa7e11c13e6facd5baea0c804b889"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f221aab5645f12b7d8187a21881906aabfdaa7e11c13e6facd5baea0c804b889"
-    sha256 cellar: :any_skip_relocation, sonoma:        "98082abc7cbc19c8a3de2069320459d7ca20c9004fb29ad3f986d02c3ff450ce"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "47da3ca485277ef48e64a67b4cdd45e657e2d44f4b84943a39b9f07027ed7d3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78f6ce73a62d0763a860bd1530dd8e590e6b2405892444fc338278711fd47891"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9fe95d11fac0ad5b703add2372bca7d04ab16355788c02f2ad1c96e2cb565184"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9fe95d11fac0ad5b703add2372bca7d04ab16355788c02f2ad1c96e2cb565184"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9fe95d11fac0ad5b703add2372bca7d04ab16355788c02f2ad1c96e2cb565184"
+    sha256 cellar: :any_skip_relocation, sonoma:        "30b51319f6a2fbbe9b4d4a0046d16bb53dbd033b0d13b920a1f4c29fc4312c63"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f0d37e7ff5744115e8761e00919540432f55eb8124583cbc2a07c01d130fedca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c9724c5d4e8d78dbdf2a4e67582d23d08067dba40898eeda52b30a5e8f09aa7"
   end
 
   depends_on "go" => :build
