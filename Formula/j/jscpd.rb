5a6945011cb11e8489ec4535dd1a4894efe7fe94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-15 13:19:18 +0000

    jscpd: update 4.2.0 bottle.

--- Formula/j/jscpd.rb
+++ Formula/j/jscpd.rb
@@ -6,7 +6,7 @@ class Jscpd < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e3a9429aa154ac031e85ba041e9ceb79a6a8b8ccffe98215d65e306467cfbfcf"
+    sha256 cellar: :any_skip_relocation, all: "fbf6f74598e855437dbffb9461a221c7828ddf0a4b49928eda80b1a55ba9182b"
   end
 
   depends_on "node"
