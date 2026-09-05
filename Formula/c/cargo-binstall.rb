c6e3db151a0f7ad51163c50351ce55bba0dbe0f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 19:55:32 +0000

    cargo-binstall 1.23.0

--- Formula/c/cargo-binstall.rb
+++ Formula/c/cargo-binstall.rb
@@ -1,8 +1,8 @@
 class CargoBinstall < Formula
   desc "Binary installation for rust projects"
   homepage "https://github.com/cargo-bins/cargo-binstall"
-  url "https://github.com/cargo-bins/cargo-binstall/archive/refs/tags/v1.22.0.tar.gz"
-  sha256 "0a8c262c86aa025a66a8945b390302193fa05c608b759ebcb989bd2304be00d7"
+  url "https://github.com/cargo-bins/cargo-binstall/archive/refs/tags/v1.23.0.tar.gz"
+  sha256 "e86f3c97853d21ac62b053b177f7920de3a3967f81042b441961087c98938fb3"
   license "GPL-3.0-only"
   head "https://github.com/cargo-bins/cargo-binstall.git", branch: "main"
 
